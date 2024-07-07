be46b22b5a8611df445ba14229284ab6e28d130b
VigneshCPandian <vignesh.c.pandian@gmail.com>
2024-07-07 04:46:32 +0100

    tf file correction

--- IaC/modules/tf-state/tf-state.tf
+++ IaC/modules/tf-state/tf-state.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "terraform_state" {
 resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
-    status = "enabled"
+    status = "Enabled"
   }
 }
 
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
   bucket = aws_s3_bucket.terraform_state.bucket
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AWS256"
+      sse_algorithm = "AES256"
     }
   }
 }
